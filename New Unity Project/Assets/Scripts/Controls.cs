af63c0cd272daf82d4d6b3eb8149d571a8d801a5
aeisley19 <52506796+aeisley19@users.noreply.github.com>
2020-01-17 17:38:12 -0500

    Added Project

---
+++ New Unity Project/Assets/Scripts/Controls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{   
+    public Rigidbody rb;
+
+    public float spd;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, spd * Time.deltaTime);
+
+       if (Input.GetKey("D"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+    }
+}
