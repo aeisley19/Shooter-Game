c8c9dfe0d4c310c571481a6c9161f150542c5666
aeisley19 <52506796+aeisley19@users.noreply.github.com>
2020-01-20 11:35:44 -0500

    Added projectiles
    God, I'm working slowly

--- New Unity Project/Assets/Scripts/Controls.cs
+++ New Unity Project/Assets/Scripts/Controls.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controls : MonoBehaviour
-{   
+{
     public Rigidbody2D rb;
-
+    public GameObject missile;
     public float spd;
 
 
@@ -17,30 +17,34 @@ public class Controls : MonoBehaviour
     void FixedUpdate()
     {
         Movement();
-
- 
     }
 
+    //Controls player movement using transform.position.
     void Movement ()
     {
-        if (Input.GetKey("d"))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.right * spd * Time.deltaTime;
         }
 
-        if (Input.GetKey("a"))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.position += Vector3.left * spd * Time.deltaTime;
         }
 
-        if (Input.GetKey("w"))
+        if (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.up * spd * Time.deltaTime;
         }
 
-        if (Input.GetKey("s"))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.position += Vector3.down * spd * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(missile, transform.position, transform.rotation);
         }
     }
 }
